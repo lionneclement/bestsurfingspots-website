89bba2536288d7085d2b4a271805b9b659a1da61
clement <lionneclement@gmail.com>
2022-05-29 23:45:49 +0200

    update navbar design

--- components/menu/NavBar.tsx
+++ components/menu/NavBar.tsx
@@ -4,23 +4,23 @@ import {LINKEDIN_LINK} from '../../data/Link';
 
 const NavBar: FunctionComponent = () => {
   return (
-    <nav className="bg-white px-2 sm:px-4 py-3 drop-shadow-md">
-      <div className="container flex flex-wrap justify-between items-center mx-auto text-primary">
+    <nav className="bg-white py-3 px-6 drop-shadow-md">
+      <div className="container flex flex-wrap justify-between items-center text-primary">
         <Link href="/">
           <a className="flex items-center">
             <span className="self-center text-xl font-semibold whitespace-nowrap">Best Surfing Spots</span>
           </a>
         </Link>
-        <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
-          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+        <div className="w-full block w-auto">
+          <ul className="flex flex-row text-sm font-medium">
             <li>
               <Link href="/">
-                <a className="block py-2 px">Home</a>
+                <a className="block p-2">Home</a>
               </Link>
             </li>
             <li>
               <Link href={LINKEDIN_LINK}>
-                <a target="_blank" className="block py-2 px-4">
+                <a target="_blank" className="block p-2">
                   Contact
                 </a>
               </Link>
--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import {FunctionComponent} from 'react';
 import Footer from '../components/menu/footer';
 import NavBar from '../components/menu/NavBar';
@@ -12,9 +11,6 @@ const DefaultLayout: FunctionComponent<Props> = ({children}) => {
   return (
     <Layout>
       <>
-        <Head>
-          <title>Kelvin - Conseils écologiques, recettes Do It Yourself, suivi de votre impact</title>
-        </Head>
         <NavBar />
         {children}
         <Footer />
