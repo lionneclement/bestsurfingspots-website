1761f5fad5acf083abe1e44f10c342e2e0111d62
lionneclement <lionneclement@gmail.com>
2022-06-12 01:02:37 +0200

    slugify add backslash

--- utils/slugify.ts
+++ utils/slugify.ts
@@ -1,6 +1,6 @@
 import originalSlugify from 'slugify';
 
-const removeMatcher = /[*+~.()'"!:@,]|[^\p{L}\p{N}\p{P}\p{Z}^$\n]/gu;
+const removeMatcher = /[*+~.()'"!:@,\\]|[^\p{L}\p{N}\p{P}\p{Z}^$\n]/gu;
 
 export const customSlugify = (string: string): string => {
   return originalSlugify(string, {remove: removeMatcher, lower: true, locale: 'us'});
