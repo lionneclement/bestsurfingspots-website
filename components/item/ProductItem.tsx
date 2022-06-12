61f2e0cef8ec363dda37bb8e92beafcc62efc6d9
lionneclement <lionneclement@gmail.com>
2022-06-12 15:45:58 +0200

    fix filter

--- components/item/ProductItem.tsx
+++ components/item/ProductItem.tsx
@@ -7,35 +7,37 @@ export const ProductItem = ({products}: {products: Product[]}) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 mt-6 relative">
       {products.map(({title, size, location, price, picture, id}, index) => {
-        return (
-          <div key={index} className="relative h-full border rounded-lg overflow-hidden">
-            <div className="relative w-full h-72 bg-gray-300">
-              <Image
-                src={picture}
-                alt={title}
-                layout="fill"
-                className="object-cover w-full relative"
-                priority={index < 8}
-              />
+        if (index < 100) {
+          return (
+            <div key={index} className="relative h-full border rounded-lg overflow-hidden">
+              <div className="relative w-full h-72 bg-gray-300">
+                <Image
+                  src={picture}
+                  alt={title}
+                  layout="fill"
+                  className="object-cover w-full relative"
+                  priority={index < 8}
+                />
+              </div>
+              <div className="px-3 pt-3">
+                <div className="flex justify-between text-gray-500">
+                  <span>Size {size}</span>
+                  <span>{location}</span>
+                </div>
+                <div className="truncate">
+                  <Link href={surfboardLink({id, title})}>
+                    <a className="stretched-link" title={title}>
+                      <span className="text-lg font-semibold my-1">{title}</span>
+                    </a>
+                  </Link>
+                </div>
+                <div className="flex justify-between mb-2">
+                  <span>{price}</span>
+                </div>
+              </div>
             </div>
-            <div className="px-3 pt-3">
-              <div className="flex justify-between text-gray-500">
-                <span>Size {size}</span>
-                <span>{location}</span>
-              </div>
-              <div className="truncate">
-                <Link href={surfboardLink({id, title})}>
-                  <a className="stretched-link" title={title}>
-                    <span className="text-lg font-semibold my-1">{title}</span>
-                  </a>
-                </Link>
-              </div>
-              <div className="flex justify-between mb-2">
-                <span>{price}</span>
-              </div>
-            </div>
-          </div>
-        );
+          );
+        }
       })}
     </div>
   );
